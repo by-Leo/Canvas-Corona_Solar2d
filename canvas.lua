8f1bb0279d055fc4ae3b616d2b688f919ab9322d
by-Leo <62703990+by-Leo@users.noreply.github.com>
2020-05-21 13:36:51 +0300

    Version 1.1

---
+++ canvas.lua
@@ -0,0 +1,42 @@
+local canvas = {}
+
+function canvas.newImage( self_group, self_path, self_dir, self_x, self_y, self_width, self_height )
+  if not self_path then
+    self_path, self_dir = self_group, system.ResourceDirectory
+  elseif not self_dir then
+    self_path, self_dir = self_group, self_path
+  end
+
+  local data
+  local file = io.open( system.pathForFile( self_path, self_dir ), 'rb' )
+  if file then
+    data = file:read('*a')
+    io.close( file )
+  end
+
+  local self_name = tostring(math.random(-999999999, 999999999))
+  local file = io.open( system.pathForFile( self_name, system.TemporaryDirectory ), 'wb' )
+  if file then
+    file:write(data)
+    io.close( file )
+  end
+
+  local a
+  if self_group then
+    a = display.newImage( self_group, self_name, system.TemporaryDirectory )
+  else
+    a = display.newImage( self_name, system.TemporaryDirectory )
+  end
+
+  if self_x or self_y then
+    a.x, a.y = self_x, self_y
+  end
+  if self_width then
+    a.width, a.height = self_width, self_height
+  end
+
+  os.remove( system.pathForFile( self_name, system.TemporaryDirectory ) )
+  return a
+end
+
+return canvas
